00e6b0d05251b16c4b4d2c4bb6030abcab60670d
Armend Kadrija <armendskadrija@gmail.com>
2022-04-11 00:30:11 +0200

    pagination on results

--- src/Application/VehiclePositions/Queries/GetNearestBusesQuery.cs
+++ src/Application/VehiclePositions/Queries/GetNearestBusesQuery.cs
@@ -1,13 +1,15 @@
-﻿using MediatR;
+using MediatR;
 using AutoMapper;
 using Journey.Application.Common.Interfaces;
 using NetTopologySuite.Geometries;
 using Microsoft.EntityFrameworkCore;
 using Application.VehiclePositions.DTOs;
-using Journey.Domain.Entities;
 using System.ComponentModel.DataAnnotations;
+using Journey.Application.Common.Models;
+using AutoMapper.QueryableExtensions;
+using Journey.Application.Common.Mappings;
 
-public class GetNearestBusesQuery : IRequest<IEnumerable<VehiclePositionDTO>>
+public class GetNearestBusesQuery : IRequest<PaginatedList<VehiclePositionDTO>>
 {
     [Required(ErrorMessage = "Valid Latitude is Required")]
     public double Latitude { get; set; }
@@ -15,10 +17,12 @@ public class GetNearestBusesQuery : IRequest<IEnumerable<VehiclePositionDTO>>
     [Required(ErrorMessage = "Valid Longitude is Required")]
     public double Longitude { get; set; }
     public int WithinDistance { get; set; } = 50;
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 20;
 
 }
 
-public class GetNearestBusesWithPaginationHandler : IRequestHandler<GetNearestBusesQuery, IEnumerable<VehiclePositionDTO>>
+public class GetNearestBusesWithPaginationHandler : IRequestHandler<GetNearestBusesQuery, PaginatedList<VehiclePositionDTO>>
 {
     private readonly IApplicationDbContext _context;
     private readonly IMapper _mapper;
@@ -29,17 +33,21 @@ public class GetNearestBusesWithPaginationHandler : IRequestHandler<GetNearestBu
         _mapper = mapper;
     }
 
-    public async Task<IEnumerable<VehiclePositionDTO>> Handle(GetNearestBusesQuery request, CancellationToken cancellationToken)
+    public async Task<PaginatedList<VehiclePositionDTO>> Handle(GetNearestBusesQuery request, CancellationToken cancellationToken)
     {
         var location = new Point(request.Longitude, request.Latitude);
 
-        var result = await _context.VehiclePositions
+        var busLocations = _context.VehiclePositions
             .Include(vehicle => vehicle.Stop)
             .Where(vehicle => vehicle.Location.IsWithinDistance(location, request.WithinDistance))
-            .OrderBy(vehicle => vehicle.Location.Distance(location))
-            .Take(30)
-            .ToListAsync();
+            .AsNoTracking()
+            .GroupBy(b => b.VehicleNumber)
+            .Select(g => g.First());
 
-        return _mapper.Map<IEnumerable<VehiclePosition>, IEnumerable<VehiclePositionDTO>>(result);
+        var mappedBusLocations = await busLocations
+            .ProjectTo<VehiclePositionDTO>(_mapper.ConfigurationProvider)
+            .PaginatedListAsync(request.PageNumber, request.PageSize);
+
+        return mappedBusLocations;
     }
 }
--- src/WebUI/Controllers/VehiclePositionController.cs
+++ src/WebUI/Controllers/VehiclePositionController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.VehiclePositions.DTOs;
+using Journey.Application.Common.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Journey.WebUI.Controllers;
@@ -6,7 +7,9 @@ namespace Journey.WebUI.Controllers;
 public class VehiclePositionController : ApiControllerBase
 {
     [HttpGet("/buses/near")]
-    public async Task<IEnumerable<VehiclePositionDTO>> GetNear([FromQuery] GetNearestBusesQuery query)
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(PaginatedList<VehiclePositionDTO>), 200)]
+    public async Task<PaginatedList<VehiclePositionDTO>> GetNear([FromQuery] GetNearestBusesQuery query)
     {
         return await Mediator.Send(query);
     }
