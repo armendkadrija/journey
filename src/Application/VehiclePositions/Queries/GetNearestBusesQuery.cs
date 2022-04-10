317423f4c0612eeac174ac6239d1068ac6cd091c
Armend Kadrija <armendskadrija@gmail.com>
2022-04-11 01:13:17 +0200

    added api documentation

--- src/Application/VehiclePositions/Queries/GetNearestBusesQuery.cs
+++ src/Application/VehiclePositions/Queries/GetNearestBusesQuery.cs
@@ -12,12 +12,23 @@ using Journey.Application.Common.Mappings;
 public class GetNearestBusesQuery : IRequest<PaginatedList<VehiclePositionDTO>>
 {
     [Required(ErrorMessage = "Valid Latitude is Required")]
+    [Display(Description = "Latitude of location")]
     public double Latitude { get; set; }
 
     [Required(ErrorMessage = "Valid Longitude is Required")]
+    [Display(Description = "Longitude of location")]
     public double Longitude { get; set; }
-    public int WithinDistance { get; set; } = 50;
+
+    [Display(Description = "Distance radius in meters (Default 500)")]
+    public int WithinDistance { get; set; } = 500;
+
+    [Display(Description = "Last known location in seconds (Default 5)")]
+    public int WithinSeconds { get; set; } = 5;
+
+    [Display(Description = "Page number (Default 1)")]
     public int PageNumber { get; set; } = 1;
+
+    [Display(Description = "Page size (Default 20)")]
     public int PageSize { get; set; } = 20;
 
 }
@@ -36,13 +47,16 @@ public class GetNearestBusesWithPaginationHandler : IRequestHandler<GetNearestBu
     public async Task<PaginatedList<VehiclePositionDTO>> Handle(GetNearestBusesQuery request, CancellationToken cancellationToken)
     {
         var location = new Point(request.Longitude, request.Latitude);
+        var secondsAgo = DateTime.Now.AddSeconds(request.WithinSeconds * -1);
 
         var busLocations = _context.VehiclePositions
             .Include(vehicle => vehicle.Stop)
-            .Where(vehicle => vehicle.Location.IsWithinDistance(location, request.WithinDistance))
-            .AsNoTracking()
-            .GroupBy(b => b.VehicleNumber)
-            .Select(g => g.First());
+            .Where(vehicle =>
+                vehicle.Location.IsWithinDistance(location, request.WithinDistance) &&
+                vehicle.TimeStamp >= secondsAgo
+            )
+            .OrderBy(vehicle => vehicle.Location.Distance(location))
+            .OrderByDescending(vehicle => vehicle.TimeStamp);
 
         var mappedBusLocations = await busLocations
             .ProjectTo<VehiclePositionDTO>(_mapper.ConfigurationProvider)
--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -11,6 +11,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
         services.AddDbContext<ApplicationDbContext>(options =>
             options.UseNpgsql(
                 configuration.GetConnectionString("DefaultConnection"),
